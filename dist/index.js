cc4623535fcc196b460f2fe5b4424b403e362255
Dennison Bertram <dennison@dennisonbertram.com>
2025-01-09 21:08:25 -0500

    Fix: Update server capabilities configuration to properly define tools

--- dist/index.js
+++ dist/index.js
@@ -24,7 +24,28 @@ async function startServer() {
         version: '0.0.1',
     }, {
         capabilities: {
-            tools: {}
+            tools: {
+                postTweet: {
+                    description: 'Post a tweet to Twitter',
+                    inputSchema: {
+                        type: 'object',
+                        properties: {
+                            text: { type: 'string', description: 'The text of the tweet' },
+                        },
+                        required: ['text'],
+                    },
+                },
+                searchTweets: {
+                    description: 'Search for tweets on Twitter',
+                    inputSchema: {
+                        type: 'object',
+                        properties: {
+                            query: { type: 'string', description: 'The query to search for' },
+                        },
+                        required: ['query'],
+                    },
+                },
+            }
         },
     });
     server.setRequestHandler(ListToolsRequestSchema, async () => {
--- src/index.ts
+++ src/index.ts
@@ -35,7 +35,28 @@ async function startServer(): Promise<void> {
         version: '0.0.1',
     }, {
         capabilities: {
-            tools: {}
+            tools: {
+                postTweet: {
+                    description: 'Post a tweet to Twitter',
+                    inputSchema: {
+                        type: 'object',
+                        properties: {
+                            text: { type: 'string', description: 'The text of the tweet' },
+                        },
+                        required: ['text'],
+                    },
+                },
+                searchTweets: {
+                    description: 'Search for tweets on Twitter',
+                    inputSchema: {
+                        type: 'object',
+                        properties: {
+                            query: { type: 'string', description: 'The query to search for' },
+                        },
+                        required: ['query'],
+                    },
+                },
+            }
         },
     });
 
